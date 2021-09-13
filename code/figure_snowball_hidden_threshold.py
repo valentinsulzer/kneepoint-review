8debd8b291b4f60dcedaaa71c6f60357fec01fae
petermattia <peter.m.attia@gmail.com>
2021-09-12 20:10:04 -0700

    Update snowball hidden threshold

--- code/figure_snowball_hidden_threshold.py
+++ code/figure_snowball_hidden_threshold.py
@@ -14,13 +14,14 @@ import config
 cycle_numbers = np.arange(500)
 
 snowball_degradation = np.exp(cycle_numbers / 150) - 1
-linear_degradation1 = cycle_numbers ** 0.5 / 1.2
-linear_degradation2 = cycle_numbers / 20
+
+hidden_degradation1 = cycle_numbers / 45
+hidden_degradation2 = np.exp(cycle_numbers / 150) - 1
 
 cycle_numbers_subset1 = np.arange(322)
-linear_degradation31 = cycle_numbers_subset1 / 100
+threshold_degradation1 = cycle_numbers_subset1 / 100
 cycle_numbers_subset2 = np.arange(322,500)
-linear_degradation32 = cycle_numbers_subset2 / 8 - 37
+threshold_degradation2 = cycle_numbers_subset2 / 8 - 37
 threshold = 3.2
 
 knee_retention = 100 - snowball_degradation
@@ -41,10 +42,10 @@ ax[0].plot(cycle_numbers, knee_retention, color="tab:blue")
 ax[1].plot(cycle_numbers, knee_retention, color="tab:purple")
 ax[2].plot(cycle_numbers, knee_retention, color="tab:red")
 ax[3].plot(cycle_numbers, snowball_degradation, "--", color="tab:blue")
-ax[4].plot(cycle_numbers, linear_degradation1, ":", color="tab:purple")
-ax[4].plot(cycle_numbers, linear_degradation2, "--", color="tab:purple")
-ax[5].plot(cycle_numbers_subset1, linear_degradation31, ":", color="tab:red")
-ax[5].plot(cycle_numbers_subset2, linear_degradation32, "--", color="tab:red")
+ax[4].plot(cycle_numbers, hidden_degradation1, ":", color="tab:purple")
+ax[4].plot(cycle_numbers, hidden_degradation2, "--", color="tab:purple")
+ax[5].plot(cycle_numbers_subset1, threshold_degradation1, ":", color="tab:red")
+ax[5].plot(cycle_numbers_subset2, threshold_degradation2, "--", color="tab:red")
 ax[5].axhline(threshold, linestyle="--", color="black")
 ax[5].annotate("threshold", (10,3.7))
 
@@ -65,8 +66,9 @@ ax[0].set_ylim([72.5, 100.1])
 ax[3].set_ylim([-0.5, 30])
 
 # Set legends
-ax[4].legend(["State 1", "State 2"], loc="upper left")
-ax[5].legend(["State 1", "State 2"], loc="upper left")
+ax[3].legend(["State 1"], loc="upper left", title="Snowball")
+ax[4].legend(["State 1", "State 2"], loc="upper left", title="Hidden")
+ax[5].legend(["State 1", "State 2"], loc="upper left", title="Threshold")
 
 # Save figure as both .PNG and .EPS
 fig.savefig(config.FIG_PATH / "snowball_hidden_threshold.png", format="png", dpi=300)

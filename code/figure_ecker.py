056b0ce162286b29cb6a100affb83d26ded07ff5
petermattia <peter.m.attia@gmail.com>
2021-12-17 22:23:45 -0800

    Fix Ecker et al colors

--- code/figure_ecker.py
+++ code/figure_ecker.py
@@ -33,36 +33,45 @@ fig, ax = plt.subplots(figsize=(config.FIG_WIDTH, config.FIG_HEIGHT * 2),
 ax = ax.ravel()
 
 # Plot
-# Since some of the series are duplicate cells, keep track of them
-previous_label = ""
+# Since some of the series are duplicate cells, keep track of labels used
+unique_labels = set()
 label_counter = 0
-for k in np.arange(len(df_capacity.columns) / 2):
-    k = int(k)
+
+n_data_series = int(len(df_capacity.columns) / 2)
+
+for k in np.arange(n_data_series):
     
-    label = df_capacity.columns[2 * k].split("%")[0] + "% DOD"
-    
+    # Capacity/resistance
     efc_capacity = df_capacity[df_capacity.columns[2 * k]][1:]
     norm_capacity = df_capacity[df_capacity.columns[2 * k + 1]][1:]
     
     efc_resistance = df_resistance[df_resistance.columns[2 * k]][1:]
     norm_resistance = df_resistance[df_resistance.columns[2 * k + 1]][1:]
     
+    # Labels and colors
+    label = df_capacity.columns[2 * k].split("%")[0] + "% DOD"
+    
+    if label not in unique_labels:
+        unique_labels.add(label)
+        label_counter += 1
+        temp_label = label
+    else:
+        temp_label = ""
+    print(f"{label}: Group {label_counter}")
+    
+    # Plot
     ax[0].plot(efc_capacity, norm_capacity, "-", marker=".",
                color = colors[label_counter],
-               label=label if label != previous_label else "")
+               label=temp_label)
     
     ax[1].plot(efc_resistance, norm_resistance, "-", marker=".",
                color = colors[label_counter],
-               label=label if label != previous_label else "")
+               label=temp_label)
     
-    print(label, previous_label, label_counter)
-    if label != previous_label:
-        label_counter += 1
-    previous_label = label
-
 
 for k in range(len(ax)):
     ax[k].set_title(chr(97 + k), loc="left", weight="bold")
+    ax[k].set_xlim(left=-90)
     l = ax[k].legend(title="Ecker et al.\nNMC/graphite cylindrical cells\n1C/1C, 35°C")
     plt.setp(l.get_title(), multialignment='center')
 
